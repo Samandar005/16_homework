06482e4ca0322b93830940281b5becd220cfdf20
Samandar Nayimjonov <SamandarNayimjonov@gmail.com>
2025-01-18 10:57:15 +0300

    UPDATE

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
-
+# GPT EMAS BITTA APPda QILGANIM UCHUN, TUSHUNARLI BULISHI UCHUN TEPASIGA KAMENT YOZIB CHIQDM!
+# HOME PAGE
 def home(request):
     selected_categories = None
     products = Product.objects.all()
@@ -22,6 +23,7 @@ def home(request):
     }
     return render(request, 'index.html', ctx)
 
+# BRAND CREATE
 def create_brand(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -36,6 +38,7 @@ def create_brand(request):
             return redirect('home')
     return render(request, 'products/brand-create.html')
 
+# CATEGORY CREATE
 def create_category(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -48,6 +51,7 @@ def create_category(request):
             return redirect('home')
     return render(request, 'products/category-create.html')
 
+# COLOR CREATE
 def create_color(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -60,55 +64,7 @@ def create_color(request):
             return redirect('home')
     return render(request, 'products/color-create.html')
 
-def product_list(request):
-    selected_brands = None
-    selected_colors = None
-    products = Product.objects.all()
-    brands = Brand.objects.all()
-    colors = Color.objects.all()
-    categories = Category.objects.all()
-
-    min_price = request.GET.get('min_price')
-    max_price = request.GET.get('max_price')
-    brand = [int(x) for x in request.GET.getlist('brand')]
-    color = request.GET.get('color')
-    sort = request.GET.get('sort')
-
-    if min_price:
-        products = products.filter(price__gte=float(min_price))
-    if max_price:
-        products = products.filter(price__lte=float(max_price))
-
-    if brand:
-        products = products.filter(brand__id__in=brand)
-        selected_brands = brands.filter(id__in=brand)
-        brands = brands.exclude(id__in=brand)
-
-    if color:
-        products = products.filter(brand__id__in=color)
-        selected_colors = colors.filter(id__in=color)
-        colors = colors.exclude(id__in=color)
-
-
-    if sort == 'low_to_high':
-        products = products.order_by('price')
-    elif sort == 'high_to_low':
-        products = products.order_by('-price')
-    elif sort == 'name_asc':
-        products = products.order_by('name')
-    elif sort == 'name_desc':
-        products = products.order_by('-name')
-
-    ctx = {
-        'products': products,
-        'brands': brands,
-        'categories': categories,
-        'colors': colors,
-        'selected_colors': selected_colors,
-        'selected_brands': selected_brands
-    }
-    return render(request, 'products/product-by-category.html', ctx)
-
+# PRODUCT CREATE
 def create_product(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -143,6 +99,57 @@ def create_product(request):
     }
     return render(request, 'products/product-create.html', ctx)
 
+
+# PRODUCT LIST
+def product_list(request):
+    selected_brands = None
+    selected_colors = None
+    products = Product.objects.all()
+    brands = Brand.objects.all()
+    colors = Color.objects.all()
+    categories = Category.objects.all()
+
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    brand = [int(x) for x in request.GET.getlist('brand')]
+    color = request.GET.get('color')
+    sort = request.GET.get('sort')
+
+    if min_price:
+        products = products.filter(price__gte=float(min_price))
+    if max_price:
+        products = products.filter(price__lte=float(max_price))
+
+    if brand:
+        products = products.filter(brand__id__in=brand)
+        selected_brands = brands.filter(id__in=brand)
+        brands = brands.exclude(id__in=brand)
+
+    if color:
+        products = products.filter(brand__id__in=color)
+        selected_colors = colors.filter(id__in=color)
+        colors = colors.exclude(id__in=color)
+
+    if sort == 'low_to_high':
+        products = products.order_by('price')
+    elif sort == 'high_to_low':
+        products = products.order_by('-price')
+    elif sort == 'name_asc':
+        products = products.order_by('name')
+    elif sort == 'name_desc':
+        products = products.order_by('-name')
+
+    ctx = {
+        'products': products,
+        'brands': brands,
+        'categories': categories,
+        'colors': colors,
+        'selected_colors': selected_colors,
+        'selected_brands': selected_brands
+    }
+    return render(request, 'products/product-by-category.html', ctx)
+
+# DETAIL PRODUCT
 def product_detail(request, year, month, day, slug):
     product = get_object_or_404(
         Product,
@@ -156,7 +163,7 @@ def product_detail(request, year, month, day, slug):
            'reviews': reviews,}
     return render(request, 'products/product-detail.html', ctx)
 
-
+# COMMENT
 def create_review(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'POST':
@@ -178,6 +185,7 @@ def create_review(request, pk):
     }
     return render(request, 'products/product-detail.html', ctx)
 
+# SUCCESS COMMENT
 def success_review(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'products/success-commented.html', {'product': product})
